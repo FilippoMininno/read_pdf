4ddd07895f9e66cfe1632252fa521c21d7c58d4b
FilippoIspanico <filippo.ispanico@gmail.com>
2026-06-14 17:39:44 +0200

    repo refactor

--- duckdb/.github/config/extensions/mysql_scanner.cmake
+++
@@ -1,9 +0,0 @@
-if (NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
-    duckdb_extension_load(mysql_scanner
-            DONT_LINK
-            LOAD_TESTS
-            GIT_URL https://github.com/duckdb/duckdb-mysql
-            GIT_TAG e2760cb11f13a12ce74e8c798c07c48deb97a577
-            SUBMODULES database-connector
-            )
-endif()
